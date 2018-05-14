23fdcb6e6732a0302cddc5ac50800235a39f5ef3
Daniel Kehoe <daniel@danielkehoe.com>
2018-05-14 17:13:38 +0800

    add a thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'thankyou', to: 'thank_you#index'
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users

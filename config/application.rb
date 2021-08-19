fc18425d4ad2dcea8643b31d20ba122455851658
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-08-19 16:06:35 +0100

    Adding in Procfile to control how Puma should start webserver

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RailsSignupThankyou
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-#    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

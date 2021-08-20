e10b09d066bff028b1e857479fff6051858f402c
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-08-20 12:47:06 +0100

    Making some changes to spec helper etc. file based off Stackoverflow comments

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,9 +1,11 @@
-describe ProductsController do
+require 'rails_helper'
+
+RSpec.describe ProductsController do
 
   describe 'GET #show' do
 
     it "returns a PDF file" do
-      get :show, id: 'product', format: 'pdf'
+     get :show, id: 'product', format: 'pdf'
       expect(response.headers['Content-Type']).to have_content 'application/pdf'
     end
 

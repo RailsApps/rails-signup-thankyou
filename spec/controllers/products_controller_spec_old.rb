ac5cacf10f208bf27e62822eb40765b660928e3e
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-08-20 16:29:02 +0100

    Making changes to rails_helper and product controller rb files and got it working locally

--- spec/controllers/products_controller_spec_old.rb
+++ spec/controllers/products_controller_spec_old.rb
@@ -5,7 +5,7 @@ RSpec.describe ProductsController do
   describe 'GET #show' do
 
     it "returns a PDF file" do
-     get :show, params: { id: 'product', format: 'pdf' }
+     get :show, id: 'product', format: 'pdf'
       expect(response.headers['Content-Type']).to have_content 'application/pdf'
     end
 

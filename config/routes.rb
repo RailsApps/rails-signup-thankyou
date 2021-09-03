c03e08078944c4c3a5ee65b3aa3c4bc923283393
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-09-03 16:39:34 +0100

    Making changes to web app to look more unique

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'thankyou', to: 'thank_you#index'
-  root to: 'visitors#index'
-  get 'products/:id', to: 'products#show', :as => :products
-  devise_for :users
-  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

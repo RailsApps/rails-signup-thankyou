568fbbd39f86a82f9e2f03333805057d0b26e8f5
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-09-03 17:40:26 +0100

    Amending routes.rb to remove duplicate line

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
   get 'thankyou', to: 'thank_you#index'
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products

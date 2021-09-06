b66aeded98128252a10a67dd673ff4072a1f8dfb
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-09-06 18:23:20 +0100

    Adding in Cypress.json and 2 Cypress directories from Sample App project

---
+++ cypress/integration/sample3_spec.js
@@ -0,0 +1,6 @@
+it('successfully loads', function() {
+  cy.request({
+      url: 'http://0.0.0.0:3000/login',
+      failOnStatusCode: false
+  })
+})
---
+++ cypress_test_examples/sample_spec.js
@@ -0,0 +1,6 @@
+describe('The signin page', function() {
+  it('successfully loads', function() {
+    cy.request('http://0.0.0.0:3000'),
+    failOnStatusCode: false
+  })
+})

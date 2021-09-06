a06fdf63ad496e20384086f212946881821db77e
Brian O'Halloran <brian_o_halloran@hotmail.com>
2021-09-06 15:51:00 +0100

    UnCommenting out devise_for in routes.rb temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'thankyou', to: 'thank_you#index'
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
-#  devise_for :users
+  devise_for :users
   resources :users
 end

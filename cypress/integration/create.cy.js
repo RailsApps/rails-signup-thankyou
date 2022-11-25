0329ce67f7045cb691ec544b10cf52d729f4cf68
Brian O'Halloran <brian_o_halloran@hotmail.com>
2022-11-25 17:10:06 +0000

    Making a lot of changes to create test to get it of a similar format to login

--- cypress/integration/create.cy.js
+++ cypress/integration/create.cy.js
@@ -1,101 +1,40 @@
-const invalidFeedback = Cypress.env('messages').invalidFeedback
-
 describe('User can navigate to create account screen', function() {
     it('from url', function() {
       cy.visit('https://rails-signup-thankyou-boh.herokuapp.com/users/sign_up')
       cy.get('#signup-title').contains('Roll on up!!')
     })
 })
+    
+Cypress.Commands.add('login', (name, username, password, confirmation) => {
+  cy.visit('https://rails-signup-thankyou-boh.herokuapp.com/users/sign_up')
+  cy.get('#user_name').type(name)
+  cy.get('#user_email').type(username)
+  cy.get('#user_password').type(password)
+  cy.get('#user_password_confirmation').type(confirmation)
+  cy.get('#new_user > input.button.right').click()
+})
+    
 
-describe('Signup is unsuccessful with 16 character username', function() {
-    beforeEach(function() {
-        const user = {
-            username: 'ohjdcbao54321098',
-            email: 'john@doe.com',
-            password: 'Password123',
-            passwordconfirmation: 'Password123'
-        }
-        cy.createAccount(user)
-    })
+describe('Signup is unsuccessful when incorrectly formatted e-mail address is provided', function() {
 
-    it('stay on signup page', function() {
-        cy.url().should('include', '/sign_up')
-    })
+  beforeEach(() => {
+    cy.login('User2 McUser', 'user2!user2.com', 'p4ssw0rd123', 'p4ssw0rd123')
+  })
 
-    it('error message visible', function() {
-        cy.get('.invalid-feedback').should('be.visible')
-        cy.get('.invalid-feedback').contains(invalidFeedback.username)
-    })
+  it('should actually be accessible', () => {
+    cy.visit('https://rails-signup-thankyou-boh.herokuapp.com/users/sign_up')
+  })
+
 })
 
-describe('Signup is unsuccessful with non-alphanumeric username', function() {
-    beforeEach(function() {
-        const user = {
-            username: 'Tom@Nook',
-            email: 'Tom',
-            password: 'Nook',
-            passwordconfirmation: 'bells'
-        }
-        cy.createAccount(user)
-    })
+describe('Signup is successful with these credentials', function() {
 
-    it('stay on signup page', function() {
-        cy.url().should('include', '/sign_up')
-    })
+  beforeEach(() => {
+    cy.login('User2 McUser', 'user2@user2.com', 'p4ssw0rd123', 'p4ssw0rd123')
+  })
 
-    it('error message visible', function() {
-        cy.get('.invalid-feedback').should('be.visible')
-        cy.get('.invalid-feedback').contains(invalidFeedback.username)
-    })
-})
+  it('should actually be accessible', () => {
+    cy.visit('https://rails-signup-thankyou-boh.herokuapp.com/users/sign_up')
+  })
 
-
-describe('User can create account', function() {
-    const uuid = () => Cypress._.random(0, 1e6)
-    const password = 'bells'
-    const firstName = 'Tom'
-    const lastName = 'Nook'
-    const expectedBalance = '$0.00'
-
-    beforeEach(function() {
-        const id = uuid()
-        const user = {
-            username: `user_${id}`,
-            firstName: firstName,
-            lastName: `${lastName}-${id}`,
-            password: password
-        }
-
-        cy.createAccount(user)
-    })
-
-    it('redirected to home', function() {
-        cy.url().should('include', '/home')
-    })
-
-    it('contain zero balance', function() {
-        cy.get('#current-balance').contains(expectedBalance)
-    })
-
-    it('sees correct username', function() {
-        cy.get('#accountDropdown').contains(`${firstName} ${lastName}`)
-    })
-
-    it('sees empty transaction history message', function() {
-        const transactionMsgs = Cypress.env('messages').transaction
-        // sees empty transaction history message
-        cy.get('#transaction-table').children('.card-table-header').contains(transactionMsgs.empty)
-        // does not see error message
-        cy.get('#transaction-table').children().should('not.contain', transactionMsgs.error)
-
-    })
-
-
-    it('sees no transactions', function() {
-        // new accounts only see empty history message
-        // new accounts should not see a table
-        cy.get('#transaction-table').children().should('have.length', 1)
-        cy.get('#transaction-table').children().should('have.class', 'card-table-header')
-        cy.get('#transaction-table').children().should('not.have.class', 'table')        
-    })
 })
--- module-b/README.md
+++ module-b/README.md
@@ -1,4 +1,4 @@
 This is a README file
 
-Edit 45
+Edit 46
 
